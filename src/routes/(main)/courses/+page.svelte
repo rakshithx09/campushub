5ad79d0b85b9b857c4070cbd7a1eedadefd657c2
Rakshith <rakshithx09@gmail.com>
2024-01-29 22:03:01 +0530

    courses page structure created

--- src/routes/(main)/courses/+page.svelte
+++ src/routes/(main)/courses/+page.svelte
@@ -1,1 +1,18 @@
-<div>rakshith</div>
+<script lang="ts">
+    const subjects = [
+      { name: 'DAA' },
+      { name: 'DBMS' },
+      { name: 'Algorithms' },
+      { name: 'Web Development' },
+      { name: 'Operating Systems' },
+    ];
+  </script>
+  
+  <section class="mt-16">
+    {#each subjects as subject (subject.name)}
+      <div class="py-6 px-8  flex items-center m-auto rounded-tl-0 rounded-br-[0.0625rem] rounded-tr-[2.4375rem] rounded-bl-[2.375rem] bg-[#3d414c]" style="width: 80%; height: 5.7rem;   margin-bottom: 1rem;">
+        {subject.name}
+      </div>
+    {/each}
+  </section>
+  
