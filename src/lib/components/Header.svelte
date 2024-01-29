9c64dfd58557637ca64dbbcd36659551ab7fc133
VarshithPawarHR <varshithpawarhr@gmail.com>
2024-01-29 23:13:33 +0530

    pocketbase

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -11,11 +11,6 @@
 <header>
   <nav>
     {#if user}
-<<<<<<< HEAD
-      <a href="/"><img src="src\lib\assets\message-icon.svg" class="msg-icon" alt="msg" /></a>
-      <a href="/courses" class="nav-link">Courses</a>
-      <a href="/resources" class="nav-link">Resources</a>
-=======
       <a href="/"
         ><img
           src="src\lib\assets\message-icon.svg"
@@ -25,27 +20,15 @@
       >
       <a href="/courses">Courses</a>
       <a href="/resources">Resources</a>
->>>>>>> 88117cfe65e6b01b39739cec9ecdaf24b84733b0
     {/if}
   </nav>
 
   <button on:click={reset} class="btn1">
-<<<<<<< HEAD
-    <img src={nitteLogo} alt="NMAMIT NITTE" class="logo-img" />
-  </button>
-
-  <div class="logo-section">
-    {#if user?.username || user?.email}
-      <Profile {user} />
-    {/if}
-  </div>
-=======
     <img src={nitteLogo} alt="NMAMIT NITTE" />
   </button>
   {#if user?.username || user?.email}
     <Profile {user} />
   {/if}
->>>>>>> 88117cfe65e6b01b39739cec9ecdaf24b84733b0
 </header>
 
 <style>
@@ -60,10 +43,6 @@
     align-items: center;
     background-color: var(--bg-accent);
     z-index: 3;
-<<<<<<< HEAD
-    transition: background-color 0.3s ease;
-=======
->>>>>>> 88117cfe65e6b01b39739cec9ecdaf24b84733b0
   }
 
   nav {
@@ -72,36 +51,6 @@
     gap: 4rem;
     font-family: "Inter", sans-serif;
     font-weight: 700;
-<<<<<<< HEAD
-  }
-
-  .nav-link {
-    display: flex;
-    align-items: center;
-    position: relative;
-    transition: color 0.3s ease;
-  }
-
-  .nav-link::before {
-    content: "";
-    position: absolute;
-    bottom: -5px;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 0;
-    height: 2px;
-    background-color: var(--primary);
-    transition: width 0.3s ease;
-  }
-
-  .nav-link:hover {
-    color: var(--primary);
-  }
-
-  .nav-link:hover::before {
-    width: 100%;
-=======
->>>>>>> 88117cfe65e6b01b39739cec9ecdaf24b84733b0
   }
 
   a {
@@ -109,26 +58,9 @@
     align-items: center;
     transition: color 0.3s ease;
   }
-<<<<<<< HEAD
-
-  a:hover {
-    color: var(--primary);
-  }
-
-  .msg-icon {
-    height: 38px;
-    transition: transform 0.3s ease;
-  }
-
-  .msg-icon:hover {
-    transform: scale(1.2);
-  }
-
-=======
   .msg-icon {
     height: 40px;
   }
->>>>>>> 88117cfe65e6b01b39739cec9ecdaf24b84733b0
   .btn1 {
     margin-left: auto;
     margin-right: 40px;
