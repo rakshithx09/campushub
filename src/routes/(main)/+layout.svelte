a4c5388681a1a46254ed2e65cec2ca5c60b8fc8a
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-02-02 23:04:14 +0530

    refactor

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,13 +1,9 @@
 <script>
   import Header from "$lib/components/Header.svelte";
-  import Footer from "$lib/components/Footer.svelte";
   import { currentUser } from "$lib/stores";
-  import Profile from "$lib/components/Profile.svelte";
 
   $: user = $currentUser;
 </script>
 
 <Header {user} />
-<!-- <Profile user={user ? user : null} /> -->
 <slot />
-<Footer/>
